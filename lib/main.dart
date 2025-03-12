33ee525988b72dfb4840d8f200411195f9cef323
Abram555 <100032096@sedu.s-mu.edu.eg>
2025-03-12 21:06:24 +0200

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +9,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: InAppWebView(
-          initialUrlRequest: URLRequest(url: Uri.parse("https://coptic-dictionry.glitch.me/")),
-        ),
-      ),
+      title: 'Flutter WebView',
+      home: WebViewPage(),
     );
   }
 }
 
+class WebViewPage extends StatefulWidget {
+  @override
+  _WebViewPageState createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the WebView controller
+    WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WebView App'),
+      ),
+      body: WebView(
+        initialUrl: 'https://coptic-dictionry.glitch.me/',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller = webViewController;
+        },
+      ),
+    );
+  }
+}
