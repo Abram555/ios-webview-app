b9ea89f93b1e43aaebd2c2a6530b9ae6149f0ce7
Abram555 <100032096@sedu.s-mu.edu.eg>
2025-03-12 20:07:43 +0200

    main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: InAppWebView(
+          initialUrlRequest: URLRequest(url: Uri.parse("httpscoptic-dictionry.glitch.me")),
+        ),
+      ),
+    );
+  }
+}
 
